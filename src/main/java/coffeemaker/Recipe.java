93f3a20a46046a990fd5d021d0e814f650c3e321
Miguel Reyes <miguel.reyes0588@gmail.com>
2024-02-03 18:33:18 -0500

    Test methods/cases complete
    Fixed defects
    
    Optimized code

--- src/main/java/coffeemaker/Recipe.java
+++ src/main/java/coffeemaker/Recipe.java
@@ -35,13 +35,13 @@ public class Recipe {
 	 * @param chocolate   The amtChocolate to set.
 	 */
     public void setAmtChocolate(String chocolate) throws RecipeException {
-    	int amtChocolate = 0;
+    	int amtChocolate;
     	try {
     		amtChocolate = Integer.parseInt(chocolate);
     	} catch (NumberFormatException e) {
     		throw new RecipeException("Units of chocolate must be a positive integer");
     	}
-		if (amtChocolate >= 0) {
+		if (amtChocolate > 0) {
 			this.amtChocolate = amtChocolate;
 		} else {
 			throw new RecipeException("Units of chocolate must be a positive integer");
@@ -54,18 +54,17 @@ public class Recipe {
 		return amtCoffee;
 	}
     /**
-	 * @param amtCoffee   The amtCoffee to set.
+	 * @param coffee   The amtCoffee to set.
 	 */
     public void setAmtCoffee(String coffee) throws RecipeException {
-    	int amtCoffee = 0;
+    	int amtCoffee;
     	try {
     		amtCoffee = Integer.parseInt(coffee);
     	} catch (NumberFormatException e) {
     		throw new RecipeException("Units of coffee must be a positive integer");
     	}
-		if (amtCoffee >= 0) {
+		if (amtCoffee > 0) {
 			this.amtCoffee = amtCoffee;
-			System.out.println(5);
 		} else {
 			throw new RecipeException("Units of coffee must be a positive integer");
 		}
@@ -77,16 +76,16 @@ public class Recipe {
 		return amtMilk;
 	}
     /**
-	 * @param amtMilk   The amtMilk to set.
+	 * @param milk   The amtMilk to set.
 	 */
     public void setAmtMilk(String milk) throws RecipeException{
-    	int amtMilk = 0;
+    	int amtMilk;
     	try {
     		amtMilk = Integer.parseInt(milk);
     	} catch (NumberFormatException e) {
     		throw new RecipeException("Units of milk must be a positive integer");
     	}
-		if (amtMilk >= 0) {
+		if (amtMilk > 0) {
 			this.amtMilk = amtMilk;
 		} else {
 			throw new RecipeException("Units of milk must be a positive integer");
@@ -99,17 +98,16 @@ public class Recipe {
 		return amtSugar;
 	}
     /**
-	 * @param amtSugar   The amtSugar to set.
+	 * @param sugar   The amtSugar to set.
 	 */
     public void setAmtSugar(String sugar) throws RecipeException {
-    	int amtSugar = 0;
+    	int amtSugar;
     	try {
     		amtSugar = Integer.parseInt(sugar);
     	} catch (NumberFormatException e) {
-			//DEFECT: Fails to per
     		throw new RecipeException("Units of sugar must be a positive integer");
     	}
-		if (amtSugar >= 0) {
+		if (amtSugar > 0) {
 			this.amtSugar = amtSugar;
 		} else {
 			throw new RecipeException("Units of sugar must be a positive integer");
@@ -139,13 +137,13 @@ public class Recipe {
 	 * @param price   The price to set.
 	 */
     public void setPrice(String price) throws RecipeException{
-    	int amtPrice = 0;
+    	int amtPrice;
     	try {
     		amtPrice = Integer.parseInt(price);
     	} catch (NumberFormatException e) {
     		throw new RecipeException("Price must be a positive integer");
     	}
-		if (amtPrice >= 0) {
+		if (amtPrice > 0) {
 			this.price = amtPrice;
 		} else {
 			throw new RecipeException("Price must be a positive integer");
@@ -172,15 +170,21 @@ public class Recipe {
 	@Override
 	public boolean equals(Object obj) {
 		//NO FUNCTIONAL DEFECTS
-		if (this == obj) //OK: == operator checks for mem location; if pt to same memory then must have same name value
+
+		//OK: == operator checks for mem location; if pt to same memory then must have same name value
+		if (this == obj)
 			return true;
-		if (obj == null) //OK: if null then nothing to compare
+		//OK: if null then nothing to compare
+		if (obj == null)
 			return false;
-		if (getClass() != obj.getClass()) //OK: if not same class at runtime then nothing to compare (edge case would be
-			// parent/child class)
+		//OK: if not same class at runtime then nothing to compare (test case would be parent/child class)
+		//we will never enter this control structure as there are no parent/child class relations with Recipe
+		if (getClass() != obj.getClass())
 			return false;
+
 		final Recipe other = (Recipe) obj;
-		// CONVOLUTED CONTROL STRUCTURES SO TEST CASES FOR MAX CODE COVERAGE IS THE CHALLENGE
+
+		// name is never null; we will never enter this control structures true flow
 		if (name == null) {
 			if (other.name != null)
 				return false;
@@ -191,14 +195,15 @@ public class Recipe {
 
 	@Override
 	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		//Formula can be optimized
+		// final int prime = 31;
+		// int result = 1;
+		// result = prime * result + ((name == null) ? 0 : name.hashCode());
+		// Formula above can be optimized
 		// name is never equal to null in lifecycle of recipe object - proof in setName()
-		// after removing conditional below we're left with following:
-		//     ( (prime * result) + name.hashCode()) where (prime * result is always 31) so can be simplified - PEMDAS
-		// Optimized formula: int result = prime * name.hashCode();
-		result = prime * result + ((name == null) ? 0 : name.hashCode());
-		return result;
+		// Using PEMDAS we can further optimize
+		// In ( (prime * result) + name.hashCode()) prime * result is always 31 so simplify to prime + name,hashCode()
+		// Prime is just a constant so no practical affect to uniqueness of hashcode so can be simplified to name.hashCode()
+		// Optimized formula: int result = name.hashCode();
+		return name.hashCode();
 	}
 }
